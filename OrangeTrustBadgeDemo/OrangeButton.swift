7c456370cf9def515dfed43461c76c8f29007298
Marc Beaudoin <marc.beaudoin@orange.com>
2016-09-11 23:37:42 +0200

    Add support for Swift 2.3

--- OrangeTrustBadgeDemo/OrangeButton.swift
+++ OrangeTrustBadgeDemo/OrangeButton.swift
@@ -134,12 +134,12 @@ public extension UIImage {
         UIGraphicsBeginImageContext(rect.size)
         let context = UIGraphicsGetCurrentContext()
         
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillRect(context, rect)
+        CGContextSetFillColorWithColor(context!, color.CGColor)
+        CGContextFillRect(context!, rect)
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return image
+        return image!
     }
 }
